b8dea6e6722f8dcbe9e5d34ab3cdc1c9641c9eb0
NAbudulloh <16523112@students.uii.ac.id>
2019-07-03 14:00:26 +0700

    silakan ditest

--- sistem_pakar.sql
+++ sistem_pakar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2019 at 10:53 AM
+-- Generation Time: Jul 03, 2019 at 08:59 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -38,25 +38,57 @@ CREATE TABLE `faktor` (
 --
 
 INSERT INTO `faktor` (`id_faktor`, `nama_faktor`) VALUES
-(1, 'daratan'),
-(2, 'daerah_tinggi'),
-(3, 'jarak_sungai'),
-(4, 'curah_hujan');
+(1, 'Daratan'),
+(2, 'Taerah Tinggi'),
+(3, 'Jarak Sungai'),
+(4, 'Curah Hujan'),
+(5, 'Risiko');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `myu`
+-- Table structure for table `hasil`
 --
 
-CREATE TABLE `myu` (
-  `id_myu` int(11) NOT NULL,
-  `myu_rendah` decimal(6,4) DEFAULT NULL,
-  `myu_sedang1` decimal(6,4) DEFAULT NULL,
-  `myu_sedang2` decimal(6,4) DEFAULT NULL,
-  `myu_tinggi` decimal(6,4) DEFAULT NULL,
-  `id_faktor` int(11) DEFAULT NULL
+CREATE TABLE `hasil` (
+  `id_hasil` int(11) NOT NULL,
+  `hasil1` int(15) DEFAULT NULL,
+  `hasil2` int(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hasil`
+--
+
+INSERT INTO `hasil` (`id_hasil`, `hasil1`, `hasil2`) VALUES
+(1, 0, 0),
+(2, 75, 25);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `masukan`
+--
+
+CREATE TABLE `masukan` (
+  `id_masukan` int(11) NOT NULL,
+  `tinggi_permukaan` int(11) DEFAULT NULL,
+  `jumlah_daerahT` int(11) DEFAULT NULL,
+  `jarak_sungai` int(11) DEFAULT NULL,
+  `curah_hujan` int(11) NOT NULL,
+  `hasil1` int(11) NOT NULL,
+  `hasil2` int(11) NOT NULL,
+  `id_hasil` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `masukan`
+--
+
+INSERT INTO `masukan` (`id_masukan`, `tinggi_permukaan`, `jumlah_daerahT`, `jarak_sungai`, `curah_hujan`, `hasil1`, `hasil2`, `id_hasil`) VALUES
+(1, 0, 0, 0, 0, 0, 0, NULL),
+(2, 50, 14, 10, 400, 75, 25, NULL),
+(3, 55, 15, 12, 450, 65, 35, NULL);
 
 -- --------------------------------------------------------
 
@@ -112,7 +144,9 @@ INSERT INTO `parameter` (`id_parameter`, `nama_parameter`, `batas_bawah`, `batas
 (13, 'dekat', 5, 15, 3),
 (14, 'sedang1', 10, 20, 3),
 (15, 'sedang2', 20, 30, 3),
-(16, 'jauh', 25, 50, 3);
+(16, 'jauh', 25, 50, 3),
+(17, 'Rendah', 10, 60, 5),
+(18, 'Tinggi', 50, 100, 5);
 
 --
 -- Indexes for dumped tables
@@ -125,44 +159,56 @@ ALTER TABLE `faktor`
   ADD PRIMARY KEY (`id_faktor`);
 
 --
--- Indexes for table `myu`
+-- Indexes for table `hasil`
 --
-ALTER TABLE `myu`
-  ADD PRIMARY KEY (`id_myu`),
-  ADD KEY `FK_myu` (`id_faktor`);
+ALTER TABLE `hasil`
+  ADD PRIMARY KEY (`id_hasil`);
+
+--
+-- Indexes for table `masukan`
+--
+ALTER TABLE `masukan`
+  ADD PRIMARY KEY (`id_masukan`),
+  ADD KEY `FK_hasil` (`id_hasil`);
 
 --
 -- Indexes for table `parameter`
 --
 ALTER TABLE `parameter`
   ADD PRIMARY KEY (`id_parameter`),
-  ADD KEY `FK_parameter` (`id_faktor`);
+  ADD KEY `FK_param` (`id_faktor`) USING BTREE;
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `myu`
+-- AUTO_INCREMENT for table `hasil`
 --
-ALTER TABLE `myu`
-  MODIFY `id_myu` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `hasil`
+  MODIFY `id_hasil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `masukan`
+--
+ALTER TABLE `masukan`
+  MODIFY `id_masukan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `myu`
+-- Constraints for table `masukan`
 --
-ALTER TABLE `myu`
-  ADD CONSTRAINT `FK_myu` FOREIGN KEY (`id_faktor`) REFERENCES `faktor` (`id_faktor`);
+ALTER TABLE `masukan`
+  ADD CONSTRAINT `FK_hasil` FOREIGN KEY (`id_hasil`) REFERENCES `hasil` (`id_hasil`);
 
 --
 -- Constraints for table `parameter`
 --
 ALTER TABLE `parameter`
-  ADD CONSTRAINT `FK_parameter` FOREIGN KEY (`id_faktor`) REFERENCES `faktor` (`id_faktor`);
+  ADD CONSTRAINT `parameter_ibfk_1` FOREIGN KEY (`id_faktor`) REFERENCES `faktor` (`id_faktor`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
